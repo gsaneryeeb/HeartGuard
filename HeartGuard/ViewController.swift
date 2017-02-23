8ca0412211d639e98b70056d89228312174b7bda
JasonZ <gsangeryeee@gmail.com>
2017-02-24 07:16:34 +0800

    Init Project
    Init Project

---
+++ HeartGuard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartGuard
+//
+//  Created by JasonZ on 2017-2-24.
+//  Copyright © 2017 saneryee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
