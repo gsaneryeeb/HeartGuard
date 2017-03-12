4373372517a6c40c421e5e9da6dfd20c35a3da02
JasonZ <gsangeryeee@gmail.com>
2017-03-13 07:14:30 +0800

    Firebase
    Added Firebase framework for Data Persistence

---
+++ Podfile
@@ -0,0 +1,9 @@
+# HeartGuard
+
+use_frameworks!
+platform :ios, '9.0'
+
+pod 'Firebase/Core'
+
+target 'HeartGuard' do
+end
---
+++ Pods/Target Support Files/Pods-HeartGuard/Pods-HeartGuard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeartGuard : NSObject
+@end
+@implementation PodsDummy_Pods_HeartGuard
+@end
