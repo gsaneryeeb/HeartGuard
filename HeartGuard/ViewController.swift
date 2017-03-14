6c29c2d5680bede3ebde7629b9a495c18e85cf8d
JasonZ <gsangeryeee@gmail.com>
2017-03-14 17:35:42 +0800

    Added Firebase Login
    Added Login with Email and Password use Firebase

--- HeartGuard/ViewController.swift
+++ HeartGuard/ViewController.swift
@@ -7,9 +7,69 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    // MARK: Login
+    @IBAction func action(_ sender: UIButton) {
+        
+        if emailText.text != "" && passwordText.text != ""
+        {
+            if segmentControl.selectedSegmentIndex == 0{
+                // Login user
+                FIRAuth.auth()?.signIn(withEmail: emailText.text!, password: passwordText.text!, completion: { (user, error) in
+                    if user != nil {
+                        // Sign in successful
+                        self.performSegue(withIdentifier: "segue", sender: self)
+                        
+                    }else{
+                        if let myError = error?.localizedDescription{
+                            
+                            print(myError)
+                        
+                        }else{
+                            // user or password error
+                            print("Error")
+                        }
+                    }
+                })
+            }else{
+                // sign up
+                FIRAuth.auth()?.createUser(withEmail: emailText.text!, password: passwordText.text!, completion: { (user, error) in
+                    if user != nil
+                    {
+                        self.performSegue(withIdentifier: "segue", sender: self)
+
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription{
+                            
+                            print(myError)
+                            
+                        }else{
+                            // user or password error
+                            print("Error")
+                        }
+                    }
+                })
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ use_frameworks!
 platform :ios, '9.0'
 
 pod 'Firebase/Core'
+pod ‘Firebase/Auth’
 
 target 'HeartGuard' do
 end
